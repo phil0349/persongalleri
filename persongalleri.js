5fbc5aff44c6e333f868b122f6218f6f2c815af6
Philip Sjøtlov <philipsjoetlov@gmail.com>
2022-08-26 11:56:43 +0200

    Persongalleri done

--- persongalleri.js
+++ persongalleri.js
@@ -1,22 +1,80 @@
-let container = document.querySelector("section");
-let temp = document.querySelector("template");
+const endpoint = "https://persongalleri-5d3e.restdb.io/rest/persongalleri";
+const mereinfo = {
+  header: {
+    "x-apikey": "600fe9211346a1524ff12e31",
+  },
+};
 
-const fil = "persongalleri.json";
-
-async function hentdata(fil) {
-  const resultat = await fetch(fil);
-  const json = await resultat.json();
+async function hentData() {
+  const response = await fetch(endpoint, mereinfo);
+  const json = await response.json();
+  console.log(json);
   vis(json);
 }
 
-function vis(personer) {
-  personer.forEach((enPerson) => {
-    let klon = temp.cloneNode(true).content;
-    klon.querySelector("h2").textContent = enPerson.navn;
-    klon.querySelector(".født").textContent = enPerson.født;
-    klon.querySelector("img").src = enPerson.billede;
+//function vis(data) {
+//  console.log(data);
+//}
+const main = document.querySelcetor("main");
+const temp = document.querySelcetor("template").content;
+
+function vis(json) {
+  //console.log(json);
+  json.forEach((enPerson) => {
+    const klon = temp.cloneNode(true).content;
+    klon.querySelector(".img").src = "faces/" + enPerson.billede;
+    klon.querySelector(".navn").textContent = enPerson.navn;
+    klon.querySelector(".email").textContent = enPerson.efternavn;
+    klon.querySelector(".fødselsdag").textContent = enPerson.fødselsdag;
+    klon.querySelector(".titel").textContent = enPerson.titel;
+    //klon.querySelector(".religion").textContent = enPerson.religion;
+    //klon.querySelector(".hobby").textContent = enPerson.hobby;
+
     container.appendChild(klon);
   });
 }
 
-hentdata(fil);
+hentData();
+
+/*  HENTE DATA FRA RESTDB.IO
+
+const endpoint = "https://persongalleri-5d3e.restdb.io/rest/persongalleri";
+
+const options = {
+  header: {
+    'x-apikey': "600fe9211346a1524ff12e31"
+  }
+};
+
+async function hentData() {
+  const response = await fetch(endpoint, options);
+  const json = await response.json();
+  console.log(json);
+  vis(json);
+}
+
+VIS JSON DATA I CONSOLEN
+
+function vis(data) {
+console.log(data);
+}
+
+function vis(dimser) {
+    console.log(dimser);
+    const container = document.querySelcetor("container");
+    const skabelon = document.querySelcetor("template").content;
+    dimser.forEach(dims => {
+        const klon = skabelon.cloneNode(true);
+    klon.querySelector(".emne").textContent = dims.emne;
+    klon.querySelector(".pris").textContent = dims.pris + " kr.";
+    klon.querySelector(".modtaget").textContent = "Modtaget d." + dims.modtaget.slice(0, 1)
+    klon.querySelector("article").style.borderRadius = "1em";
+    container.appendChild(klon);
+  });
+}
+    }
+}
+
+hentData();
+
+*/
